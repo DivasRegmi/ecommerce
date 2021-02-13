a0bac09834b6e8d7b9cb6c52976fe3a3e0b8d672
divas regmi <divasregmi@gmail.com>
2021-02-13 16:33:02 +0545

    starting point for admin react

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,32 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import OftadehRoutes from './components/OftadehRoutes/OftadehRoutes';
+
 import { ThemeProvider } from '@material-ui/core/styles';
-import getTheme from './oftadeh-configs/themeConfig';
-import ThemeContext from './context/ThemeContext';
+
+import Routes from './Routes';
+import theme from './theme';
 
 const App = () => {
-  const curThemeName = localStorage.getItem('appTheme') || 'light';
-
-  const [themeType, setThemeType] = useState(curThemeName);
-
-  const setThemeName = (themeName) => {
-    localStorage.setItem('appTheme', themeName);
-    setThemeType(themeName);
-  };
-
-  const theme = getTheme({
-    paletteType: themeType,
-  });
-
   return (
-    <ThemeContext.Provider value={{ setThemeName, curThemeName }}>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <OftadehRoutes />
-        </div>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Routes />
+      </div>
+    </ThemeProvider>
   );
 };
 
